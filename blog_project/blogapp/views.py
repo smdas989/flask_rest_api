dfa345638fc454ae423074fb24e52271fbb8a7f1
soumya@989 <smdas989@gmail.com>
2022-01-12 09:59:06 +0530

    username and email validations

--- blog_project/blogapp/views.py
+++ blog_project/blogapp/views.py
@@ -12,60 +12,85 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
 import jwt
 import datetime
-
+from email_validator import validate_email, EmailNotValidError
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-	username = request.json.get('username')
-	password = request.json.get('password')
-	email = request.json.get('email')
+    username = request.json.get('username')
+    password = request.json.get('password')
+    email = request.json.get('email')
 
-	user = User.add_user(username, email, password)
+    if username is None or password is None:
+        response = Response("Enter Username and password", 201, mimetype='application/json') # missing arguments
+        return response
 
-	if username is None or password is None:
-		abort(400) # missing arguments
-	
-	# user = User(username = username)
-	# user.hash_password(password)
-	db.session.add(user)
-	db.session.commit()
-	
-	response = Response("User added", 201, mimetype='application/json')
-	return response
+    try:
+        # Validate.
+        validate_email(email)
+        
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        response = Response(str(e), 201, mimetype='application/json')
+        return response
+        
+    
+    users = User.query.all()
+    for user in users:
+        if user.email == email:
+            response = Response("Email is already taken", 201, mimetype='application/json')
+            return response # Unique email id
+        if user.username == username:
+            response = Response("Username is already taken", 201, mimetype='application/json')
+            return response # Unique username id
+    
+    user = User.add_user(username, email, password)
+
+    
+    # user = User(username = username)
+    # user.hash_password(password)
+    if user:
+        db.session.add(user)
+        db.session.commit()
+    
+        response = Response("User added Successfully" + "\n username:" + username + "\n email:" + email, 201, mimetype='application/json')
+    
+    else:
+        response = Response("User registration failed", 404, mimetype='application/json')
+    return response
 
 @app.route('/get_all_user', methods=['GET'])
 def get_all_user():
-	return_value = User.get_all_users()
-	return jsonify(return_value)
+    return_value = User.get_all_users()
+    return jsonify(return_value)
     
 
 @app.route('/get_user/<int:id>', methods=['GET'])
 def get_user(id):
-	return_value = User.get_user(id)
-	return jsonify(return_value)
+    return_value = User.get_user(id)
+    return jsonify(return_value)
     
 
 @app.route('/login', methods=['GET', 'POST'])  
 def login_user(): 
  
-	# auth = request.authorization  
+    # auth = request.authorization  
 
-	username = request.json.get('username')
-	password = request.json.get('password') 
+    username = request.json.get('username')
+    password = request.json.get('password') 
 
-	if not username or not password:  
-		# return jsonify({'message': 'could not verify'})
-		
-		return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})    
+    if not username or not password:  
+        # return jsonify({'message': 'could not verify'})
+        
+        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})    
 
-	user = User.query.filter_by(username=username).first()   
-		
-	# if check_password_hash(password, user.password):  
-	token = jwt.encode({'public_id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])  
-	return jsonify({'token' : token.decode('UTF-8')}) 
+    user = User.query.filter_by(username=username).first()   
+        
+    # if check_password_hash(password, user.password):  
+    token = jwt.encode({'public_id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])  
+    return jsonify({'token' : token.decode('UTF-8')}) 
 
-	
-	# return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
+    
+    # return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
 
 # @app.route("/")
@@ -80,11 +105,11 @@ def home(current_user):
 
     # sort_by = request.json.get('sort_by')
 
-	return_value = Post.get_all_posts()
+    return_value = Post.get_all_posts()
 
-	return jsonify({"posts":return_value,
-	"sort_by":request.json.get('sort_by')
-	}) 
+    return jsonify({"posts":return_value,
+    "sort_by":request.json.get('sort_by')
+    }) 
 
     # if search_post and sort_by:
     #     posts = Post.query.filter(Post.title.ilike('%' + search_post + '%'))
@@ -106,53 +131,53 @@ def home(current_user):
     #     #     posts = Post.query.outerjoin(PostLike).group_by(Post.id).order_by(func.count().desc(), Post.date_posted.desc()).paginate(page=page, per_page=5)
     #     elif sort_by == 'newest':
     #         posts = posts = Post.query.order_by(Post.date_posted.desc())
-	
-	
+    
+    
 @app.route("/post/new", methods=['GET', 'POST'])
 @token_required
 def new_post(current_user):
-	title = request.json.get('title')
-	content = request.json.get('content') 
-	post = Post(title=title, content=content, user_id=current_user.id)
-	db.session.add(post)
-	db.session.commit()
-	flash('Your post has been created','success')
-	return {'title': title,'content':content}
+    title = request.json.get('title')
+    content = request.json.get('content') 
+    post = Post(title=title, content=content, user_id=current_user.id)
+    db.session.add(post)
+    db.session.commit()
+    flash('Your post has been created','success')
+    return {'title': title,'content':content}
 
 @app.route('/get_post', methods=['POST'])
 @token_required
 def get_post(current_user):
-	id = request.json.get('id')
-	return_value = Post.get_post(id)
-	return jsonify(return_value)
+    id = request.json.get('id')
+    return_value = Post.get_post(id)
+    return jsonify(return_value)
     
 
 @app.route("/post/update", methods=['GET','POST'])
 @token_required
 def update_user_post(current_user):
-	request_data = request.get_json()
-	post = Post.query.get_or_404(request_data['id'])
-	
-	if post.user_id!= current_user:
-		abort(403)
+    request_data = request.get_json()
+    post = Post.query.get_or_404(request_data['id'])
+    
+    if post.user_id!= current_user:
+        abort(403)
 
-	Post.update_post(request_data['id'], request_data['title'], request_data['content'])    
-	response = Response("Post Updated", status=200, mimetype='application/json')
-	return response
+    Post.update_post(request_data['id'], request_data['title'], request_data['content'])    
+    response = Response("Post Updated", status=200, mimetype='application/json')
+    return response
 
 
 @app.route("/post/delete", methods=['POST'])
 @token_required
 def delete_post(current_user):
-	request_data = request.get_json()
-	post = Post.query.get_or_404(request_data['id'])
+    request_data = request.get_json()
+    post = Post.query.get_or_404(request_data['id'])
 
-	if post.user_id != current_user:
-		abort(403)	
+    if post.user_id != current_user:
+        abort(403)	
 
-	Post.delete_post(request_data['id'])    
-	response = Response("Post deleted", status=200, mimetype='application/json')
-	return response
+    Post.delete_post(request_data['id'])    
+    response = Response("Post deleted", status=200, mimetype='application/json')
+    return response
 
 
 # @app.route("/account", methods=['GET', 'POST'] )
@@ -255,16 +280,16 @@ def like_action(current_user, post_id, action):
         current_user.like_post(post)
         db.session.commit()
         msg = "Successfully like the post"
-	
+    
     if action == 'unlike':
         current_user.unlike_post(post)
         db.session.commit()
         msg = "Successfully unlike the post"
     
     return {'msg': msg,
-			'user':current_user.username,
-			'post':post.title
-			}
+            'user':current_user.username,
+            'post':post.title
+            }
 
 
 @app.route('/comment/<int:post_id>', methods=['GET', 'POST'])
@@ -293,32 +318,32 @@ def comment_delete(current_user, comment_id=None, post_id=None):
 @app.route('/follow/<username>')
 @token_required
 def follow(current_user, username):
-	user = User.query.filter_by(username=username).first_or_404()
-	if user is None:
-		Response('User %s not found.' % username, status=404, mimetype='application/json')
-	if user == current_user:
-		Response('You can\'t follow yourself!', status=404, mimetype='application/json')
+    user = User.query.filter_by(username=username).first_or_404()
+    if user is None:
+        Response('User %s not found.' % username, status=404, mimetype='application/json')
+    if user == current_user:
+        Response('You can\'t follow yourself!', status=404, mimetype='application/json')
 
-	u = current_user.follow(user)
-	if u is None:
-		Response('Cannot follow ' + username + '.', status=404, mimetype='application/json')
-    	
-	db.session.add(u)
-	db.session.commit()
-	response = Response("You are now following "+username, status=200, mimetype='application/json')
-	return response
+    u = current_user.follow(user)
+    if u is None:
+        Response('Cannot follow ' + username + '.', status=404, mimetype='application/json')
+        
+    db.session.add(u)
+    db.session.commit()
+    response = Response("You are now following "+username, status=200, mimetype='application/json')
+    return response
 
 @app.route('/unfollow/<username>')
 @token_required
 def unfollow(current_user, username):
     user = User.query.filter_by(username=username).first_or_404()
     if user is None:
-	    Response('User %s not found.' % username, status=404, mimetype='application/json')
+        Response('User %s not found.' % username, status=404, mimetype='application/json')
     if user == current_user:
-	    Response('You can\'t unfollow yourself!', status=404, mimetype='application/json')
+        Response('You can\'t unfollow yourself!', status=404, mimetype='application/json')
     u = current_user.unfollow(user)
     if u is None:
-	    Response('Cannot unfollow ' + username + '.', status=404, mimetype='application/json')
+        Response('Cannot unfollow ' + username + '.', status=404, mimetype='application/json')
     db.session.add(u)
     db.session.commit()
     response = Response("You have successfully unfollowed "+username, status=200, mimetype='application/json')
